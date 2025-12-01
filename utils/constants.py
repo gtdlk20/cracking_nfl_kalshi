bae347bbd81c4813cc1d54f5bebe611593d31407
gtdlk20 <gjkaufmann@optonline.net>
2025-11-30 22:27:18 -0500

    relocate data and add vader scores

--- utils/constants.py
+++ utils/constants.py
@@ -8,3 +8,4 @@ KALSHI_API_KEY = os.getenv("KALSHI_API_KEY")
 KALSHI_DATA_DAY = "data/nfl_hisoric_candlestick_day.pkl"
 KALSHI_DATA_HOUR = "data/nfl_hisoric_candlestick_hour.pkl"
 KALSHI_DATETIME_COLS = ['end_period_ts']
+REDDIT_DATA_PATH = "data/subs_sentiment"
